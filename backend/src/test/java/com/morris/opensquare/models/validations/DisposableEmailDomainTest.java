72552777053b42680a14c8f8992dd95edd115cd1
Wali Morris <walimmorris@gmail.com>
2024-02-08 16:23:19 -0800

    make DisposableEmailDomainTest class and test resource json

---
+++ backend/src/test/java/com/morris/opensquare/models/validations/DisposableEmailDomainTest.java
@@ -0,0 +1,58 @@
+package com.morris.opensquare.models.validations;
+
+import com.morris.opensquare.TestHelper;
+import org.bson.types.ObjectId;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+@SpringBootTest
+@ActiveProfiles({"coverage"})
+class DisposableEmailDomainTest {
+    private static final String DOMAIN_NAME_1 = "disposableDomain.net";
+    private static final String DOMAIN_NAME_2 = "anotherDisposableDomain.net";
+    private static final int TIMESTAMP = 123456789;
+    private static final String DISPOSABLE_DOMAIN_FILE = "backend/src/test/resources/models/DisposableEmailDomain.json";
+
+
+    private static DisposableEmailDomain disposableEmailDomain;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        disposableEmailDomain = (DisposableEmailDomain) TestHelper.convertModelFromFile(DISPOSABLE_DOMAIN_FILE, DisposableEmailDomain.class, null);
+    }
+
+    @Test
+    void getDomainName() {
+        assertEquals(DOMAIN_NAME_1, disposableEmailDomain.getDomainName());
+    }
+
+    @Test
+    void getId() {
+        // We can ensure the time stamp from the mock DisposableEmailDomain is correct
+        // from thr ObjectId
+        assertEquals(TIMESTAMP, disposableEmailDomain.getId().getTimestamp());
+    }
+
+    @Test
+    void setDomainName() {
+        disposableEmailDomain.setDomainName(DOMAIN_NAME_2);
+        assertNotEquals(DOMAIN_NAME_1, disposableEmailDomain.getDomainName());
+        assertEquals(DOMAIN_NAME_2, disposableEmailDomain.getDomainName());
+    }
+
+    @Test
+    void setId() {
+        ObjectId testId = new ObjectId();
+        disposableEmailDomain.setId(testId);
+        assertNotEquals(TIMESTAMP, disposableEmailDomain.getId().getTimestamp());
+        assertEquals(testId.getTimestamp(), disposableEmailDomain.getId().getTimestamp());
+        assertEquals(testId, disposableEmailDomain.getId());
+    }
+}
