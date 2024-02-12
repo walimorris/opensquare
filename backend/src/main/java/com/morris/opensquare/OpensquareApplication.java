47048991d055961d094055330e415243e136d13d
Wali Morris <walimmorris@gmail.com>
2024-02-12 14:06:36 -0800

    add mongoTemplate and runcommand in AtlasFunctionService

--- backend/src/main/java/com/morris/opensquare/OpensquareApplication.java
+++ backend/src/main/java/com/morris/opensquare/OpensquareApplication.java
@@ -3,15 +3,17 @@ package com.morris.opensquare;
 import com.morris.opensquare.services.AtlasFunctionService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class OpensquareApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OpensquareApplication.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(OpensquareApplication.class, args);
+		AtlasFunctionService template = applicationContext.getBean(AtlasFunctionService.class);
 
 		// test kotlin setup in project
-		new AtlasFunctionService().sayHello();
+		template.func();
 	}
 
 }
--- backend/src/main/kotlin/com/morris/opensquare/services/AtlasFunctionService.kt
+++ backend/src/main/kotlin/com/morris/opensquare/services/AtlasFunctionService.kt
@@ -1,11 +1,18 @@
 package com.morris.opensquare.services
 
+import org.bson.BsonDocument
+import org.bson.BsonInt64
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.stereotype.Service
 
 @Service
-class AtlasFunctionService {
+class AtlasFunctionService @Autowired constructor(val mongoTemplate: MongoTemplate){
 
-    fun sayHello() {
-        println("Kotlin says Waddup");
+    fun func() {
+        val command = BsonDocument("dbStats", BsonInt64(1))
+        val result = mongoTemplate.db.runCommand(command)
+        println("Function from AtlasFunction: kotlin")
+        println("dbstats: " + result.toJson())
     }
 }
