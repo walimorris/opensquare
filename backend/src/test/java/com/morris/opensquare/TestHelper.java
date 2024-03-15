f4031b6b2d04a83b23862f17dc4deccb9a80801a
Wali Morris <walimmorris@gmail.com>
2024-03-15 08:02:02 -0700

    add ObjectMapper in TestHelper.java as a reusable static field

--- backend/src/test/java/com/morris/opensquare/TestHelper.java
+++ backend/src/test/java/com/morris/opensquare/TestHelper.java
@@ -36,8 +36,10 @@ public class TestHelper {
     private static final String AGE_RANGE_3 = "36-45";
     private static final String AGE_RANGE_4 = "46-55";
 
-    // TODO: add testhelper global context
-    private TestHelper() {}
+    private static final ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
+
+    private TestHelper() {
+    }
 
     /**
      * More complicated objects such as objects that are based on a List or Map collection
@@ -59,23 +61,21 @@ public class TestHelper {
      * @throws IOException exception
      */
     public static Object convertModelFromFile(String fileName, Class<?> clazz, Class<?> type) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JavaTimeModule());
         if (clazz.isAssignableFrom(List.class)) {
             if (type == NSLookupFootPrint.class) {
                 TypeReference<List<NSLookupFootPrint>> reference = new TypeReference<>() {};
-                return mapper.readValue(new File(fileName), reference);
+                return objectMapper.readValue(new File(fileName), reference);
             }
             if (type == Result.class) {
                 TypeReference<List<Result>> reference = new TypeReference<>() {};
-                return mapper.readValue(new File(fileName), reference);
+                return objectMapper.readValue(new File(fileName), reference);
             }
             if (type == YouTubeTranscribeSegment.class) {
                 TypeReference<List<YouTubeTranscribeSegment>> reference = new TypeReference<>() {};
-                return mapper.readValue(new File(fileName), reference);
+                return objectMapper.readValue(new File(fileName), reference);
             }
         }
-        return mapper.readValue(new File(fileName), clazz);
+        return objectMapper.readValue(new File(fileName), clazz);
     }
 
     /**
@@ -90,9 +90,7 @@ public class TestHelper {
      * @throws IOException exception
      */
     public static <T> T convertModelFromFile(String fileName, Class<T> clazz) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JavaTimeModule());
-        return mapper.readValue(new File(fileName), clazz);
+        return objectMapper.readValue(new File(fileName), clazz);
     }
 
     /**
@@ -103,9 +101,7 @@ public class TestHelper {
      * @return {@link JSONObject}
      */
     public static JSONObject getJSONFromObject(Object object) {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JavaTimeModule());
-        return mapper.convertValue(object, JSONObject.class);
+        return objectMapper.convertValue(object, JSONObject.class);
     }
 
     /**
@@ -117,8 +113,6 @@ public class TestHelper {
      * @throws JsonProcessingException processing error
      */
     public static String writeValueAsString(Object object) throws JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
         return objectMapper.writeValueAsString(object);
     }
 
