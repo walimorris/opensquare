af066db3a761e857701995365bfa5a359cec1b65
Wali Morris <walimmorris@gmail.com>
2024-03-20 23:17:39 -0700

    add session attribute evaluation method for global notifications

--- backend/src/main/java/com/morris/opensquare/controllers/NotificationController.java
+++ backend/src/main/java/com/morris/opensquare/controllers/NotificationController.java
@@ -23,6 +23,10 @@ public class NotificationController {
     private final LoggerService loggerService;
     private final NotificationService notificationService;
 
+    private final static long REEVALUATE = 2;
+    private final static String GLOBAL_NOTIFICATION_INSERT_TIME_MILLIS = "global-notification-insert-time-millis";
+    private final static String GLOBAL_NOTIFICATIONS = "global-notifications";
+
     @Autowired
     public NotificationController(LoggerService loggerService, NotificationService notificationService) {
         this.loggerService = loggerService;
@@ -41,14 +45,19 @@ public class NotificationController {
     @SuppressWarnings("unchecked")
     public ResponseEntity<List<GlobalNotification>> getAllGlobalNotifications(HttpServletRequest httpServletRequest) {
         HttpSession httpSession = httpServletRequest.getSession();
-        List<GlobalNotification> sessionAllNotifications = (List<GlobalNotification>) httpSession.getAttribute("globalNotifications");
+        List<GlobalNotification> sessionAllNotifications = (List<GlobalNotification>) httpSession.getAttribute(GLOBAL_NOTIFICATIONS);
+
         if (sessionAllNotifications != null) {
-            return ResponseEntity.ok()
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .body(sessionAllNotifications);
+            if (!shouldConductSessionContentEvaluation(httpSession)) {
+                return ResponseEntity.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(sessionAllNotifications);
+            }
         }
+        LOGGER.info("Reevaluating session content for notifications");
         List<GlobalNotification> allNotifications = notificationService.readAllGlobalNotifications();
-        httpSession.setAttribute("globalNotifications", allNotifications);
+        httpSession.setAttribute(GLOBAL_NOTIFICATIONS, allNotifications);
+        httpSession.setAttribute(GLOBAL_NOTIFICATION_INSERT_TIME_MILLIS, System.currentTimeMillis());
         return ResponseEntity.ok()
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(allNotifications);
@@ -60,5 +69,33 @@ public class NotificationController {
         return ResponseEntity.ok()
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(globalNotification);
+    }
+
+    /**
+     * Session data is stored for the duration of a session. There are cases where UI
+     * calls an API and returns the current session data. That data may have been
+     * updated in the persistence layer. In order to respond with the most up-to-date
+     * data, while not stressing the data-layer, this method determines if the session
+     * store should release and reevaluate session attributes for most the up-to-date
+     * information. This reevaluation time can vary, based on the REEVALUATE class
+     * constant.
+     *
+     * @param session {@link HttpSession}
+     *
+     * @return boolean
+     */
+    private boolean shouldConductSessionContentEvaluation(HttpSession session) {
+        if (session.getAttribute(GLOBAL_NOTIFICATION_INSERT_TIME_MILLIS) != null) {
+            long insertTime = (long) session.getAttribute(GLOBAL_NOTIFICATION_INSERT_TIME_MILLIS);
+            long runningTime = System.currentTimeMillis() - insertTime;
+            long runningTimeInMinutes = (runningTime / 1000) / 60;
+            LOGGER.info("Global Notifications last evaluated '{}' minutes ago", runningTimeInMinutes);
+
+            if (runningTimeInMinutes >= REEVALUATE) {
+                session.removeAttribute(GLOBAL_NOTIFICATION_INSERT_TIME_MILLIS);
+                return true;
+            }
+        }
+        return false;
     }
 }
